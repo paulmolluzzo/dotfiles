393e47c0361a2f32ec4d76252eefe1fd8b59d01c
Paul Molluzzo <paul@molluzzo.com>
2017-01-04 10:04:22 -0500

    Add color test command

--- aliases/random.sh
+++ aliases/random.sh
@@ -101,6 +101,20 @@ get_callables() {
         | sed -E 's/(function )?([a-zA-Z0-9]*)\(\).*/\2/'
 }
 
+# print colors
+colors() {
+    for x in 8; do for i in `seq 30 37`; do for a in `seq 40 47`; do echo -ne "\e[$x;$i;$a""m\\\e[$x;$i;$a""m"; done; echo; done; done; echo "";
+    echo -e "\033[0;37mWHITE \t\033[0;30mBLACK"
+    echo -e "\033[1;30mGRAY \t\033[1;37mLIGHT_WHITE"
+    echo -e "\033[0;34mBLUE \t\033[1;34mLIGHT_BLUE"
+    echo -e "\033[0;32mGREEN \t\033[1;32mLIGHT_GREEN"
+    echo -e "\033[0;36mCYAN \t\033[1;36mLIGHT_CYAN"
+    echo -e "\033[0;31mRED \t\033[1;31mLIGHT_RED"
+    echo -e "\033[0;35mPURPLE \t\033[1;35mLIGHT_PURPLE"
+    echo -e "\033[0;33mYELLOW \t\033[1;33mLIGHT_YELLOW"
+    echo -e ""
+}
+
 # binding to return to vim quickly
 stty susp undef
 bind '"\C-z":"fg\015"'
