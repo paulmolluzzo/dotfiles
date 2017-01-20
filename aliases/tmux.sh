668089b0a7303ced4cfd45995d6f5e7068f86ce7
Paul Molluzzo <paul@molluzzo.com>
2017-01-20 10:11:21 -0500

    Specify TERM for tmux to fix xo/chalk color issues

--- aliases/tmux.sh
+++ aliases/tmux.sh
@@ -5,9 +5,9 @@
 ################################
 
 # tmux new -s
-alias tmn='tmux new -s'
+alias tmn='TERM=screen-256color-bce tmux new -s'
 # tmux attach -t
-alias tma='tmux attach -t'
+alias tma='TERM=screen-256color-bce tmux attach -t'
 # tmux ls
 alias tml='tmux ls'
 # tmux kill-session -t
