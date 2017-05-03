137f77f9cae239c3054a3e620599164cf9814db5
Paul Molluzzo <paul@molluzzo.com>
2017-05-03 13:30:10 -0400

    Brew install script

---
+++ brew.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+brew install bash
+brew install colordiff
+brew install ffmpeg
+brew install gcc@4.8
+brew install git
+brew install gnupg
+brew install httpie
+brew install irssi
+brew install isl@0.11
+brew install jq
+brew install libksba
+brew install mongodb
+brew install mysql
+brew install perl
+brew install postgresql
+brew install potrace
+brew install python
+brew install python3
+brew install reattach-to-user-namespace
+brew install ruby
+brew install ssh-copy-id
+brew install tmux
+brew install tree
+brew install vim
+brew install wget
