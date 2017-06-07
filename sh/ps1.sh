148c379ed6e8a0d24af5872f89f1f37a62bed4b7
Paul Molluzzo <paul@molluzzo.com>
2017-06-07 12:22:57 -0400

    Simplify prompt

--- sh/ps1.sh
+++ sh/ps1.sh
@@ -6,7 +6,7 @@ PathShort="\w"
 
 # Custom prompt
 
-export PS1=$Prompt_IBlue'{{}}'$Prompt_Color_Off$Prompt_BYellow' ['$PathShort']'$Prompt_Color_Off'$(git branch &>/dev/null;\
+export PS1=$Prompt_BYellow'['$PathShort']'$Prompt_Color_Off'$(git branch &>/dev/null;\
 if [ $? -eq 0 ]; then \
   echo "$(echo `git status` | grep "nothing to commit" > /dev/null 2>&1; \
   if [ "$?" -eq "0" ]; then \
@@ -15,8 +15,8 @@ if [ $? -eq 0 ]; then \
   else \
     # @5 - Changes to working tree
     echo "'$Prompt_IRed'"$(__git_ps1 " (%s)")'$Prompt_Color_Off'; \
-  fi) '$Prompt_White'\n\$'$Prompt_Color_Off' "; \
+  fi) '$Prompt_White'\$'$Prompt_Color_Off' "; \
 else \
   # @2 - Prompt when not in GIT repo
-  echo "'$Prompt_White' \n$ '$Prompt_Color_Off'"; \
+  echo "'$Prompt_White' $ '$Prompt_Color_Off'"; \
 fi)'
